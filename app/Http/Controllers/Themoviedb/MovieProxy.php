aa0b3d2f7bd2950075f3d326beea453d1a995865
Stacey <sachungo1@gmail.com>
2018-08-16 16:10:13 +0300

    Added the movie proxy

---
+++ app/Http/Controllers/Themoviedb/MovieProxy.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Themoviedb;
+
+use GuzzleHttp\Client;
+
+class MovieProxy
+{
+    protected $apiClient;
+    protected $apiKey;
+
+    public function __construct()
+    {
+        $this->apiClient = new Client([
+            'base_uri' => env(THEMOVIEDB_API_URL)
+        ]);
+        $this->apiKey = env(THEMOVIEDB_API_KEY);
+    }
+
+    public function getMoviesList($pageNumber = 1)
+    {
+        $query = $this->constructQueryString([
+            'sort_by' => 'popularity.desc',
+            'include_adult' => false,
+            'include_video' => false,
+            'page' => $pageNumber
+        ]);
+
+        // TODO: put request in a try catch handler for guzzle request errors
+        $response = $this->apiClient->get('discover/movie' . $query);
+
+        // TODO: use a resource that have only the data I need
+        return json_decode($response, true);
+    }
+
+    private function constructQueryString($queryArgs = [])
+    {
+        $params = array_merge($queryArgs, [
+            'api_key' => $this->apiKey
+        ]);
+        return http_build_query($params);
+    }
+}
