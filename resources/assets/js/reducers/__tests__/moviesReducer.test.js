387d2bb2a58ad623bb1a0d1d5df27e4a32bcbf6e
andela-sachungo <sachungo1@gmail.com>
2018-08-19 20:18:37 +0300

    Added reducer test

---
+++ resources/assets/js/reducers/__tests__/moviesReducer.test.js
@@ -0,0 +1,65 @@
+import actionTypes from '../../moviesConstants';
+import reducer from '../moviesReducer';
+
+describe('ALL movies reducer', () => {
+  let initialState;
+  beforeEach(() => {
+    initialState = {
+      movies: [],
+      nextPage: 1,
+      loading: true,
+      error: ''
+    };
+  });
+
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual(initialState);
+  });
+
+  it('should handle LOADING_ALL_MOVIES', () => {
+    expect(reducer(initialState, {
+      type: actionTypes.LOADING_ALL_MOVIES,
+      loading: false
+    })).toEqual({
+      movies: [],
+      nextPage: 1,
+      loading: false,
+      error: ''
+    });
+  });
+
+  it('should handle FETCH_ALL_MOVIES_SUCCESS', () => {
+    const state = {...initialState, loading: false};
+    expect(reducer(state, {
+      type: actionTypes.FETCH_ALL_MOVIES_SUCCESS,
+      payload: {
+        page: 2,
+        movies: [{
+          id: 176565,
+          title: 'Testing movies'
+        }]
+      }
+    })).toEqual({
+      movies: [{
+        id: 176565,
+        title: 'Testing movies'
+      }],
+      nextPage: 2,
+      loading: false,
+      error: ''
+    });
+  });
+
+  it('should handle FETCH_ALL_MOVIES_ERROR', () => {
+    const state = {...initialState, loading: false};
+    expect(reducer(state, {
+      type: actionTypes.FETCH_ALL_MOVIES_ERROR,
+      payload: 'An error occurred'
+    })).toEqual({
+      movies: [],
+      nextPage: 1,
+      loading: false,
+      error: 'An error occurred'
+    });
+  });
+});
