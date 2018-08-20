b895846f2a1e64953d1d07cc16fd8ffc617119da
Stacey <sachungo1@gmail.com>
2018-08-20 13:52:49 +0300

    Add the wrapper test

---
+++ resources/assets/js/components/movies/__tests__/MoviesWrapper.test.js
@@ -0,0 +1,23 @@
+import MoviesWrapper from '../MoviesWrapper';
+
+describe('The MoviesWrapper component', () => {
+  let props;
+  beforeEach(() => {
+    props = {
+      nextPage: 2,
+      fetchAll: jest.fn(),
+      loading: true
+    }
+  });
+   it('renders without throwing an error with minimal props', () => {
+    expect(() => shallow(<MoviesWrapper {...props} />)).not.toThrow();
+  });
+   it('renders the loading spinner when loading is true', () => {
+    const wrapper = shallow(<MoviesWrapper {...props} />);
+    expect(wrapper.find('[data-test="movies-loader"]')).toExist();
+  });
+   it('renders the MoviesWrapper component when loading is false', () => {
+    const wrapper = shallow(<MoviesWrapper {...props} loading={false} />);
+    expect(wrapper.find('[data-test="movies-list"]')).toExist();
+  });
+});
