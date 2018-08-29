bfcd76b510518e69d8ebd9187160610e669b3b16
Stacey <sachungo1@gmail.com>
2018-08-29 15:22:56 +0300

    Changed color name

--- resources/assets/js/components/shared/colors.js
+++ resources/assets/js/components/shared/colors.js
@@ -2,5 +2,6 @@ export default {
   primary: '#00ced1',
   text: '#636b6f',
   border: '#e5e5e5',
-  loadingTransparent: 'rgba(0, 206, 209, 0.1)'
+  translucent: 'rgba(0, 206, 209, 0.1)',
+  white: '#ffffff'
 }
