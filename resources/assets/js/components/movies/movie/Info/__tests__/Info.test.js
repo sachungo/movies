b9cb39c0d8d8fc4bc26472ffe51c92a1c1330f4a
Stacey <sachungo1@gmail.com>
2019-02-12 17:00:07 +0300

    Refactored info test

--- resources/assets/js/components/movies/movie/Info/__tests__/Info.test.js
+++ resources/assets/js/components/movies/movie/Info/__tests__/Info.test.js
@@ -20,29 +20,17 @@ describe('The Info component', () => {
     expect(() => shallow(<Info {...props} />)).not.toThrow();
   });
 
-  it('renders a movie image when the data prop is populated', () => {
-    props.data = {
-      poster_path: '/6etryuersdtyrty.jpg'
-    }
-    const wrapper = shallow(<Info {...props} />);
-    expect(wrapper.find('[data-test="movie-image"]')).toExist();
-  });
-
-  it('renders the title, popularity and release date when the data prop is not empty', () => {
+  it('renders movie data', () => {
     props.data = {
       title: 'Movie information',
       popularity: 456.76,
-      release_date: '2018-05-25'
-    }
-    const wrapper = shallow(<Info {...props} />);
-    expect(wrapper.find('[data-test="movie-basics"]')).toExist();
-  });
-
-  it('renders the movie overview', () => {
-    props.data = {
+      release_date: '2018-05-25',
+      poster_path: '/6etryuersdtyrty.jpg',
       overview: 'Testing movie render'
     }
     const wrapper = shallow(<Info {...props} />);
+    expect(wrapper.find('[data-test="movie-basics"]')).toExist();
+    expect(wrapper.find('[data-test="movie-image"]')).toExist();
     expect(wrapper.find('[data-test="movie-overview"]')).toExist();
   });
 
